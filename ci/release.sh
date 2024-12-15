54402bec9a8cadfd8f9b4af550c16543edd2ebe1
mark <mr.catborg@gmail.com>
2024-12-15 01:12:34 -0500

    ci: build v0.2.0-alpha

--- ci/release.sh
+++ ci/release.sh
@@ -10,10 +10,10 @@ LAST_MSG=$(git log -1 --pretty=%B)
 git add .
 
 # If last commit was a version commit, amend it. Otherwise create new commit
-if [[ $LAST_MSG =~ ^build:\ release\ v[0-9]+\.[0-9]+\.[0-9]+(-[a-zA-Z0-9]+)?$ ]]; then
+if [[ $LAST_MSG =~ ^ci:\ build\ v[0-9]+\.[0-9]+\.[0-9]+(-[a-zA-Z0-9]+)?$ ]]; then
     git commit --amend --no-edit
 else
-    git commit -m "build: release $VERSION"
+    git commit -m "ci: build $VERSION"
 fi
 
 # Clean up old tag if it exists
@@ -21,6 +21,6 @@ git tag -d $VERSION 2>/dev/null || true
 git push origin :refs/tags/$VERSION 2>/dev/null || true
 
 # Create and push new tag
-git tag -a $VERSION -m "build: release $VERSION"
+git tag -a $VERSION -m "release: $VERSION"
 git push origin main --force # Need force since we might amend
 git push origin $VERSION
